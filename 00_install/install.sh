ff6b1117938b5fdf73f38180bad7a1c9c7548f8c
Jinwoo Kim <jinwookim@kw.ac.kr>
2022-12-24 00:52:26 +0900

    update

--- 00_install/install.sh
+++ 00_install/install.sh
@@ -1,1 +1,17 @@
-sudo apt install golang-go make vim tmux -y
+# basics
+sudo apt install golang-go vim tmux -y
+
+# libbpf and libbpf-go dependencies
+sudo apt install clang make libelf-dev linux-tools-5.15.0-56-generic -y
+
+git submodule init
+git submodule update
+
+# install libbpf
+cd libbpf; cd src; make
+KG_CONFIG_PATH=/build/root/lib64/pkgconfig
+DESTDIR=/build/root
+sudo make install
+
+cd ../../libbpf-go
+make all
